41851c2795d83f7fa1941f202241a6aab90eccca
Paul Rohde <paulrohde675@gmail.com>
2023-06-25 13:58:41 +0000

    init ipynb

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv('/workspaces/axa_city_bike/data/raw/201801-citibike-tripdata.csv')
+
+# load data by looping over all 12 shards
+df_list = []
+for i in range(2):
+    filename = f'/workspaces/axa_city_bike/data/raw/2018{i+1:02d}-citibike-tripdata.csv'
+    df_list.append(pd.read_csv(filename))
+    print(filename)
+df = pd.concat(df_list)
+
 print(df)
+df.shape
