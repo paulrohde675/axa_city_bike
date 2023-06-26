73d60ac1cdf170ac2b2e213aab772ac5623128a0
Paul Rohde <paulrohde675@gmail.com>
2023-06-26 15:04:19 +0000

    Add data to feather func

--- read_data_to_feather.py
+++ read_data_to_feather.py
@@ -3,11 +3,10 @@ import pandas as pd
 
 # load data by looping over all 12 shards
 df_list = []
-for i in range(2):
+for i in range(12):
     filename = f'/workspaces/axa_city_bike/data/raw/2018{i+1:02d}-citibike-tripdata.csv'
     df_list.append(pd.read_csv(filename))
     print(filename)
 df = pd.concat(df_list)
-
-print(df)
-df.shape
+df = df.reset_index(drop=True)
+df.to_feather('/workspaces/axa_city_bike/data/raw/2018-citibike-tripdata.feather')
